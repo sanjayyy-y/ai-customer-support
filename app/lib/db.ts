a2c958c44c8a5ce06547dcb6469ccf001381619b
sanjaysahu7 <sanjaypradeep111@gmail.com>
2026-04-14 17:58:30 +0530

    feat: add mongoose dependency and implement database connection utility with global caching

---
+++ app/lib/db.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const mongo_url = process.env.MONGODB_URI
+
+if(!mongo_url) {
+    throw new Error("Please provide MONGODB_URI in the environment variables")
+}
+
+let cached = global.mongoose
+
+if(!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
+const connectDB = async () => {
+    if(cached.conn) {
+        return cached.conn
+    }
+
+    if(!cached.promise) {
+        cached.promise = mongoose.connect(mongo_url).then((conn) => conn.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise
+    }catch(error) {
+        cached.promise = null
+        throw error
+    }
+}
+
+export default connectDB
+    
